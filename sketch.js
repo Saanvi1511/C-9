c2dab01fec66f4148887c26ad060ef22a739165b
Saanvi1511 <129280325+Saanvi1511@users.noreply.github.com>
2023-03-29 17:59:03 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,20 +1,39 @@
-var box ;
-
+var box
 function setup() {
   createCanvas(400,400);
-  box = createSprite(200 , 200 , 100 , 30);
+  background(51);
+  box = createSprite(200,200,30,30);
+
 }
 
 function draw() 
 {
-  background(30);
- if (keyIsDown (RIGHT_ARROW)) {
-  box.position.x = box.position.x + 2;
- }
+
+
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    background("red");
+   
+  }
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+
+  }
 
   drawSprites();
 }
-
-
-
-
